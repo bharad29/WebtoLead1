fbe93cd906fb60c3275760c76d78388df610aa38
bharad29 <bharad2912@gmail.com>
2025-02-20 22:24:31 +0530

    recaptcha changes

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ function beforesubmit (){
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value",inputdate.value);
 
-} 
+}
+
+function timestamp()
+{
+     var response = document.getElementById("g-recaptcha-response"); 
+     if (response == null || response.value.trim() == "") 
+        {
+            var elems = JSON.parse(
+                document.getElementsByName("captcha_settings")[0].value
+            );
+            elems["ts"] = JSON.stringify(new Date().getTime());
+            document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+         } } setInterval(timestamp, 500); 
